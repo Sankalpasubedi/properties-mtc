7f1a532e1c59e8a489b477d172837c74b213e9c4
Sankalpa <sankalpasubedi007@gmail.com>
2026-05-23 20:48:11 +0545

    feat/update folder structure

---
+++ controllers/LocationController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Controllers;
+
+use Models\Property;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Message\ResponseInterface as Response;
+
+class LocationController
+{
+
+}
