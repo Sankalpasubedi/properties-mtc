382ac5d1b806c464943f914bb9e95c112391c86b
Sankalpa <sankalpasubedi007@gmail.com>
2026-05-23 20:48:11 +0545

    feat/update folder structure

---
+++ controllers/ApiSyncController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Controllers;
+
+use Models\Property;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Message\ResponseInterface as Response;
+
+class ApiSyncController
+{
+
+    private const API_BASE = 'https://trial.craig.mtcserver15.com';
+    private const API_KEY = '2S7rhsaq9X1cnfkMCPHX64YsWYyfe1he';
+}
